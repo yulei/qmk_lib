5b5172219d21d2959ff17b7785076e21e948bf95
Lei Yu <yuleiz@gmail.com>
2023-10-07 11:18:32 +0800

    added hook for raw hid

--- portable/qmk_driver.c
+++ portable/qmk_driver.c
@@ -29,6 +29,9 @@ int8_t sendchar(uint8_t c)
     return (int8_t)SEGGER_RTT_PutChar(0, c);
 }
 
+__attribute__((weak))
+void raw_hid_send_kb(uint8_t *data, uint8_t length) {}
+
 #ifdef VIAL_ENABLE
 #include "usb_common.h"
 #ifdef TINYUSB_ENABLE
@@ -36,6 +39,7 @@ int8_t sendchar(uint8_t c)
 
 void raw_hid_send(uint8_t *data, uint8_t length)
 {
+    raw_hid_send_kb(data, length);
     if (tud_hid_n_ready(ITF_NUM_VIAL)) {
         tud_hid_n_report(ITF_NUM_VIAL, 0, data, length);
     } else {
@@ -45,6 +49,7 @@ void raw_hid_send(uint8_t *data, uint8_t length)
 #include "amk_usb.h"
 void raw_hid_send(uint8_t *data, uint8_t length)
 {
+    raw_hid_send_kb(data, length);
     if (amk_usb_itf_ready(HID_REPORT_ID_VIAL)) {
         amk_usb_itf_send_report(HID_REPORT_ID_VIAL, data, length);
     } else {
